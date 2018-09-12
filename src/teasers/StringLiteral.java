d7eaef0caad3c5c69df4c8c9d0060e6c793c492b
Sharafat Ahmed <asabir24@gmail.com>
2018-09-12 19:37:14 +0600

    StringLiteral understanding

---
+++ src/teasers/StringLiteral.java
@@ -0,0 +1,27 @@
+package teasers;
+
+public class StringLiteral {
+	public static void main(String [] args) {
+		String str1  = "string";
+		String str2 = "string";
+		String strobj1 = new String("string");
+		
+		String partStr1 = "str";
+		String partStr2 = "ing";
+		
+		System.out.println(str1 == str2);
+		System.out.println(str1.equals(str2));
+		
+		System.out.println(str2 == strobj1);
+		System.out.println(str2.equals(strobj1));
+		
+		System.out.println(str1 == (partStr1 + partStr2));
+		System.out.println(str1.equals(partStr1 + partStr2));
+		
+		String parts = (partStr1 + partStr2).intern();
+		
+		System.out.println(str1 == parts);
+		System.out.println(str1.equals(parts));
+		
+	}
+}
