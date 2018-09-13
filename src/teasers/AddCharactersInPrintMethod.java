9d0838b60b070b4da9f57a9339cda84f5fc46444
Sharafat Ahmed <asabir24@gmail.com>
2018-09-13 13:29:33 +0600

    Character addition output added and explained

---
+++ src/teasers/AddCharactersInPrintMethod.java
@@ -0,0 +1,15 @@
+package teasers;
+
+/**
+ * What will be the output?
+ * @author Sabir
+ *
+ */
+public class AddCharactersInPrintMethod {
+
+	public static void main(String[] args) {
+		System.out.println('S' + 'a' + 'b' + 'i' + 'r');
+		System.out.println("S" + 'a' + 'b' + 'i' + 'r');
+	}
+
+}
---
+++ src/teasers/AddCharactersInPrintMethod.md
@@ -0,0 +1,6 @@
+## Result
+497
+Sabir
+
+##Explanation
+Characters are unsigned 16 bit variables. They behave like integers when we add, substract, multiply or divide them.
