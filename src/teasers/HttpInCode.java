de67ac533e43646cfa25d1a805acfdbebdfdbb7f
Sabir001 <s.a.sabir@hotmail.com>
2018-09-16 18:21:22 +0600

    Http link in comment explained

---
+++ src/teasers/HttpInCode.java
@@ -0,0 +1,16 @@
+package teasers;
+
+/**
+ * Will the code compile successfully?
+ * If yes, why?
+ * @author Sabir
+ */
+public class HttpInCode {
+    public static void main(String[] args){
+        System.out.println("First line");
+
+        https://facebook.com
+
+        System.out.println("Second Line");
+    }
+}
---
+++ src/teasers/HttpInCode.md
@@ -0,0 +1,5 @@
+## Result 
+Yes. It will compile successfully.
+
+## Reason
+```` http: ```` will be treated as a label name. And after ```` // ```` part, it will be treated as comment. So, there is no problem why this code should not compile.
